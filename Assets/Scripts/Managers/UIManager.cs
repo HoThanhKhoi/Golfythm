5c2f7fbdb29d2aaafc502f98af5916b78959b9fc
HoThanhKhoi <th.khoi2003@gmail.com>
2024-05-25 18:51:12 +0700

    UI_InProgress

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    
+    IEnumerator WaitForSecondsToLoadScene(float seconds, int sceneNumber)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        SceneManager.LoadScene(sceneNumber);
+    }
+    public void StartGame()
+    {
+        StartCoroutine(WaitForSecondsToLoadScene(0.75f, 1));
+    }
 }
